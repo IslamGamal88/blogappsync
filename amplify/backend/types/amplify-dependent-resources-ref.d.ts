2946580d4ec758e10042f4eb510ade522cce6ed8
Islam Abdelkader <islam.abdelkader@lendinvest.com>
2023-04-04 12:04:07 +0200

    amplify setup

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "blogappsync": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
